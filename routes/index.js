f5c99ff64943cd44f8b86895aaa058c1448b48a8
Seth <itz.saga@gmail.com>
2017-12-22 15:34:03 -0600

    Pug templating intro

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  // res.send('Hey! It works!');
+  res.render('hello', {
+    name: 'seth',
+    dog: req.query.dog
+  });
 });
 
 router.get('/reverse/:name', (req, res) => {

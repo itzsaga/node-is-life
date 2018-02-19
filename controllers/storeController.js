11a044104010b3116a909c17eed4522398bbb988
Seth <itz.saga@gmail.com>
2018-02-18 20:37:59 -0600

    Added a model

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,11 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-  req.name = 'Seth';
-  if(req.name === 'Seth') {
-    throw Error('Dumb name');
-  }
-  next();
-}
-
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('hello');

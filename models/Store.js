f25846bcf7b986151cf3a9154f034f48cc272d16
Seth <sethalexander@pm.me>
2018-10-26 14:43:11 -0500

    Prettier-ed

--- models/Store.js
+++ models/Store.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-const slug = require('slugs');
+const slug = require("slugs");
 
 const storeSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
-    required: 'Please enter a store name!'
+    required: "Please enter a store name!"
   },
   slug: String,
   description: {
@@ -16,8 +16,8 @@ const storeSchema = new mongoose.Schema({
   tags: [String]
 });
 
-storeSchema.pre('save', function(next) {
-  if(!this.isModified('name')) {
+storeSchema.pre("save", function(next) {
+  if (!this.isModified("name")) {
     next(); // skip it
     return; // stop this function from running
     // also fine:
@@ -28,4 +28,4 @@ storeSchema.pre('save', function(next) {
   // TODO make more resilient so slugs are unique
 });
 
-module.exports = mongoose.model('Store', storeSchema);
+module.exports = mongoose.model("Store", storeSchema);

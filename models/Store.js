324c96445013976b4eca5fc67de839eb213ef5d7
Seth <sethalexander@pm.me>
2018-12-28 20:29:20 -0600

    Add incrementing slugs

--- models/Store.js
+++ models/Store.js
@@ -35,7 +35,7 @@ const storeSchema = new mongoose.Schema({
   photo: String
 });
 
-storeSchema.pre("save", function(next) {
+storeSchema.pre("save", async function(next) {
   if (!this.isModified("name")) {
     next(); // skip it
     return; // stop this function from running
@@ -43,6 +43,13 @@ storeSchema.pre("save", function(next) {
     // return next();
   }
   this.slug = slug(this.name);
+  // find other stores that have the same slug
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i')
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx })
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`
+  }
+
   next();
   // TODO make more resilient so slugs are unique
 });

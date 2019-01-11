7d2cc23e3af1d2364d17629a93ffbf1b11d82223
Seth <sethalexander@pm.me>
2019-01-11 11:09:25 -0600

    Wire up forgot logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require("passport");
+const crypto = require("crypto");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 exports.login = passport.authenticate("local", {
   failureRedirect: "/login",
@@ -20,4 +23,25 @@ exports.isLoggedIn = (req, res, next) => {
   }
   req.flash("error", "Oops you must be logged in to do that!");
   res.redirect("/login");
+};
+
+exports.forgot = async (req, res) => {
+  // 1. See if a user with that email exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    req.flash("error", "No account with that email exists");
+    // req.flash("error", "A password reset has been mailed to");
+    return res.redirect("/login");
+  }
+  // 2. Set reset tokens and expiry on their account
+  user.resetPasswordToken = crypto.randomBytes(20).toString("hex");
+  user.resetPasswordExpires = Date.now() + 3600000;
+  await user.save();
+  // 3. Send them an email with the token
+  const resetUrl = `http://${req.headers.host}/account/reset/${
+    user.resetPasswordToken
+  }`;
+  req.flash('success', `You have been emailed a password reset link. ${resetUrl}`)
+  // 4. Redirect to login page
+  res.redirect('/login')
 };
